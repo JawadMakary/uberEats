3f3033221f58f341ec1afa22b960fc54d1f13b80
jawad <jawadhalbymakary@gmail.com>
2021-09-20 18:34:22 +0300

    added headerTabs

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import HeaderTabs from "../components/HeaderTabs";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
