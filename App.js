3975e4da6162a1bd2fe0223ab3d243d9cf1489f6
jawad <jawadhalbymakary@gmail.com>
2021-09-26 23:57:52 +0300

    workin on menuitem

--- App.js
+++ App.js
@@ -5,5 +5,5 @@ import Home from "./screens/Home";
 import RestaurantDetail from "./screens/RestaurantDetail";
 
 export default function App() {
-  return <Home />;
+  return <RestaurantDetail />;
 }
