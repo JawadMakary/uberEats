c6c2de2e1dd4bcfbed018ec18db4615d2be44ed2
jawad <jawadhalbymakary@gmail.com>
2021-09-27 16:39:38 +0300

    fixed viewcart bug

--- components/restaurantDetails/MenuItems.js
+++ components/restaurantDetails/MenuItems.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { Divider } from "react-native-elements";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const foods = [
   {
@@ -52,6 +53,12 @@ export default function MenuItems({ restaurantName }) {
         checkboxValue: checkboxValue,
       },
     });
+  const cartItems = useSelector(
+    (state) => state.cartReducer.selectedItems.items
+  );
+  const isFoodInCart = (food, cartItems) =>
+    Boolean(cartItems.find((item) => item.title === food.title));
+
   return (
     <View>
       {foods.map((food, index) => (
@@ -61,6 +68,7 @@ export default function MenuItems({ restaurantName }) {
               iconStyle={{ borderColor: "lightgray", borderRadius: 0 }}
               fillColor="green"
               onPress={(checkboxValue) => selectItem(food, checkboxValue)}
+              isChecked={isFoodInCart(food, cartItems)}
             />
             <FoodInfo food={food} />
             <FoodImage food={food} />
