423584fffb6b93ca88744909f6e381b2f2ea0fa8
jawad <jawadhalbymakary@gmail.com>
2021-09-28 17:40:52 +0300

    working on order completed page

--- screens/OrderCompleted.js
+++ screens/OrderCompleted.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
-import { useSelector } from "react-redux";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 
+import { useSelector } from "react-redux";
+import LottieView from "lottie-react-native";
 export default function OrderCompleted() {
   const { items, restaurantName } = useSelector(
     (state) => state.cartReducer.selectedItems
@@ -14,10 +15,31 @@ export default function OrderCompleted() {
     currency: "USD",
   });
   return (
-    <SafeAreaView style={{ marginTop: 50 }}>
-      <Text>
-        Your Order at {restaurantName} has been placed for {totalUSD}
-      </Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          margin: 15,
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <LottieView
+          style={{ height: 100, alignSelf: "center", marginBottom: 30 }}
+          source={require("../assets/animations/check-mark.json")}
+          autoPlay
+          speed={0.5}
+          loop={false}
+        />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Your order at {restaurantName} has been placed for {totalUSD}$
+        </Text>
+        <LottieView
+          style={{ height: 200, alignSelf: "center" }}
+          source={require("../assets/animations/cooking.json")}
+          autoPlay
+          speed={0.5}
+        />
+      </View>
     </SafeAreaView>
   );
 }
